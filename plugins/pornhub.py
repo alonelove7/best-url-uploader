57998ce20eb8d9f7c62a2717870a5e1c884867a1
X-Noid <67302057+X-Gorn@users.noreply.github.com>
2021-03-31 13:56:05 +0900

    Update pornhub.py

--- plugins/pornhub.py
+++ plugins/pornhub.py
@@ -1,1 +1,27 @@
+# the logging things
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
+import os
+import sqlite3
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+from pyrogram.types.bots_and_keyboards import InlineKeyboardButton, InlineKeyboardMarkup
+
+from helper_funcs.chat_base import TRChatBase
+
+@pyrogram.Client.on_message(pyrogram.filters.regex(pattern=".*xnxx.*"))
+async def xnxx(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/xnxx")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text="No Porn!",
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
