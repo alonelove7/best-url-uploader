85085cbc2e70fd92b44b709f50375f73ce0c4443
X-Noid <67302057+X-Gorn@users.noreply.github.com>
2021-04-02 17:29:00 +0900

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -249,7 +249,7 @@ async def youtube_dl_call_back(bot, update):
                 await bot.send_audio(
                     chat_id=update.message.chat.id,
                     audio=download_directory,
-                    caption=description + f"\n\nSubmitted by [{update.from_user.mention}](tg://user?id={update.from_chat_id.mention})\nUploaded by @xurluploaderbot",
+                    caption=description + f"\n\nSubmitted by [{update.from_user.mention}](tg://user?id={update.from_user_id.mention})\nUploaded by @xurluploaderbot",
                     parse_mode="HTML",
                     duration=duration,
                     # performer=response_json["uploader"],
@@ -272,7 +272,7 @@ async def youtube_dl_call_back(bot, update):
                     chat_id=update.message.chat.id,
                     document=download_directory,
                     thumb=thumb_image_path,
-                    caption=description + f"\n\nSubmitted by [{update.from_user.mention}](tg://user?id={update.from_chat_id.mention})\nUploaded by @xurluploaderbot",
+                    caption=description + f"\n\nSubmitted by [{update.from_user.mention}](tg://user?id={update.from_user_id.mention})\nUploaded by @xurluploaderbot",
                     parse_mode="HTML",
                     # reply_markup=reply_markup,
                     reply_to_message_id=update.message.reply_to_message.message_id,
@@ -308,7 +308,7 @@ async def youtube_dl_call_back(bot, update):
                 await bot.send_video(
                     chat_id=update.message.chat.id,
                     video=download_directory,
-                    caption=description + f"\n\nSubmitted by [{update.from_user.mention}](tg://user?id={update.from_chat_id.mention})\nUploaded by @xurluploaderbot",
+                    caption=description + f"\n\nSubmitted by [{update.from_user.mention}](tg://user?id={update.from_user_id.mention})\nUploaded by @xurluploaderbot",
                     parse_mode="HTML",
                     duration=duration,
                     width=width,
