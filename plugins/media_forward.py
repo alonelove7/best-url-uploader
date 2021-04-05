313268709404b537fa729e3cb9d7f0ada0904e23
X-Noid <67302057+X-Gorn@users.noreply.github.com>
2021-04-05 19:24:29 +0900

    Update media_forward.py

--- plugins/media_forward.py
+++ plugins/media_forward.py
@@ -19,8 +19,9 @@ else:
 
 @pyrogram.Client.on_message(pyrogram.filters.media)
 async def fwdmedia(bot, update):
+    bot_id = await bot.get_me()
     await bot.forward_messages(
     chat_id=Config.TRACK_CHANNEL,
-    from_chat_id=update.from_user.id,
+    from_chat_id=f"{bot_id}",
     message_ids=update.message_id
 )
